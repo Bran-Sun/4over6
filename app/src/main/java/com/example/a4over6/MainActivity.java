b0d7a6c7fa26f5f94992557df7e255bb280c0cf1
sunzhenbo <szb16@mails.tsinghua.edu.cn>
2019-05-04 21:48:18 +0800

    网络检查步骤

--- app/src/main/java/com/example/a4over6/MainActivity.java
+++ app/src/main/java/com/example/a4over6/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.a4over6;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText _ipv6, _port;
+    private Button _link, _unlink;
+    private TextView _status; //when starting, checking the network status
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -16,9 +24,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        initView();
+
+        checkNetwork();
+
+    }
+
+    protected void checkNetwork() { //check whether in ipv6 status, if it is than clickable
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    protected void initView() {
+        _ipv6 = (EditText)findViewById(R.id.editText2);
+        _port = (EditText)findViewById(R.id.editText3);
+        _link = (Button)findViewById(R.id.button);
+        _unlink = (Button)findViewById(R.id.button2);
+        _status = (TextView)findViewById(R.id.textView_status);
+
+        _link.setClickable(false); //first check the network status;
+
     }
 
     /**
